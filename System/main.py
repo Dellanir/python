928b5ea5056210262d981f7c5bcc66c85cf5b3d4
Emma Zachara <emma.zachara@gmail.com>
2019-01-12 15:58:57 +0100

    Web architecture base

---
+++ System/main.py
@@ -0,0 +1,14 @@
+import sys
+reload(sys)
+sys.setdefaultencoding('utf8')
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route('/index')
+@app.route('/')
+def index():
+   return render_template('index.html')
+
+if __name__ == '__main__':
+   app.run(debug = True)
+
