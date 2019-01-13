a3e6016e262c03f8bbf11c4530fa22f1d8887685
Emma Zachara <emma.zachara@gmail.com>
2019-01-13 13:05:37 +0100

    Added config file and configparser lib requirement

--- System/main.py
+++ System/main.py
@@ -2,6 +2,8 @@ import sys
 reload(sys)
 sys.setdefaultencoding('utf8')
 from flask import Flask, render_template
+import configparser
+
 app = Flask(__name__)
 
 @app.route('/index')
@@ -10,5 +12,10 @@ def index():
    return render_template('index.html')
 
 if __name__ == '__main__':
-   app.run(debug = True)
+   config = configparser.ConfigParser()
+   config.read('config.ini')
+   app.run( debug = config['SERVER']['debug'],
+            port = config['SERVER']['port'],
+            host = config['SERVER']['host']
+               )
 
