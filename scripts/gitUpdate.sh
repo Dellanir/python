8679d0e3dc11be5e48f03d897b6a4635b372f6c8
Emma Zachara <emma.zachara@gmail.com>
2019-01-16 17:29:08 +0100

    git update script sh

--- scripts/gitUpdate.sh
+++ scripts/gitUpdate.sh
@@ -2,4 +2,4 @@
 
 git fetch -p && git stash && git rebase && git stash pop
 
-cd .. && source venv/bin/activate && cd System && python main.py
+cd .. && source venv/bin/activate && pip install -r requirements.txt
